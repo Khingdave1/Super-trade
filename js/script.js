1b435e701b0c5e73098d3bdecb2ff1ceb4e2d198
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2021-07-04 21:44:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-// Show Register Modal
+/*===== MENU TOGGLE =====*/
+const navList = document.querySelector(".nav-list");
+const toggleMenu = document.querySelector(".toggle-menu");
+
+toggleMenu.addEventListener("click", () => {
+  navList.classList.toggle("open-menu");
+  toggleMenu.classList.toggle("open-menu");
+});
+
+/*===== SHOW REGISTER MODAL =====*/
 const regBtn = document.querySelector(".register-btn");
 const regArea = document.querySelector(".register-area");
 const regForm = document.querySelector(".register");
@@ -7,8 +16,8 @@ regBtn.addEventListener("click", () => {
   regArea.classList.add("show-modal");
 });
 
-// Close Modal
-//Hide the popup if you click outside the div
+/*===== CLOSE REGISTER MODAL =====*/
+// Hide the popup if you click outside the div
 window.addEventListener("click", (e) => {
   if (!regBtn.contains(e.target) && !regForm.contains(e.target)) {
     regArea.classList.remove("show-modal");
